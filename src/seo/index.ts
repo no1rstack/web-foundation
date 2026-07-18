5e779ee774804993057bbaefb99d4ab32232d795
no1r <info@noirstack.com>
2026-07-17 20:03:51 -0400

    Export route manifest and browser metadata APIs

--- src/seo/index.ts
+++ src/seo/index.ts
@@ -3,3 +3,5 @@ export * from './metadata.js';
 export * from './structured-data.js';
 export * from './breadcrumbs.js';
 export * from './link-analysis.js';
+export * from './route-manifest.js';
+export * from './browser-metadata.js';
